14c35d19ea753f7ff0af797ea237caf28322442d
isaquaresma-hash <isa.quaresma@fnp.org.br>
2026-09-04 14:00:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -148,8 +148,6 @@ df_base = carregar_dados()
 def set_bg_hack():
     bin_str = None
     mime_type = "image/jpeg"
-    
-    # Busca direta pelo arquivo anexado
     arquivo_fundo = "simulador.png.jpg"
     
     if os.path.exists(arquivo_fundo):
@@ -317,11 +315,12 @@ class PDFMemoria(FPDF):
             self.set_fill_color(10, 54, 99)
             self.rect(0, 0, 210, 32, "F")
 
-        self.set_y(10)
-        self.set_font("Arial", "B", 15)
+        # AJUSTE DO TÍTULO: Afastado da logo (X=65) e mantido na posição vertical superior (Y=4)
+        self.set_xy(65, 4)
+        self.set_font("Arial", "B", 13)
         self.set_text_color(255, 255, 255)
-        self.cell(0, 10, txt_pdf("MEMÓRIA DE CÁLCULO DE CONTRIBUIÇÃO"), 0, 1, "C")
-        self.set_y(40)
+        self.cell(140, 10, txt_pdf("MEMÓRIA DE CÁLCULO DE CONTRIBUIÇÃO"), 0, 1, "L")
+        self.set_y(38)
 
 
 def gerar_pdf_simulacao(municipio, uf, porte, cenario, parcelas, val_integral, valor_total, valor_parcela, economia):
